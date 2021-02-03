341ea7d107f10a1cbdde1572feb0ba8cde0482b3
fruchti <fruchti@posteo.de>
2021-02-03 19:15:26 +0100

    improved movement behavior, added basic Buff managment

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
 from kivy.properties import ObjectProperty, NumericProperty, BoundedNumericProperty, StringProperty
 
 import random
 
-class Mate(Button):
+class Mate(FloatLayout):
     ''' the base class for characters, the mages moving on the PlayingField '''
     # you have to declare the properties at class level, not at init, in order to get expected behaviour
     max_t = 100
@@ -32,27 +33,87 @@ class Mate(Button):
         self.mana = self.max_mana
         self.t = random.randint(0,99)
 
+    def change_health(self, damage, heal):
+        self.health = self.health - damage + heal
+        if self.health < 0.:
+            self.die()
+        if self.health > self.max_health:
+            self.health = self.max_health
+
+    def change_mana(self, manacost, manaboost):
+        self.mana = self.mana - manacost + manaboost
+        if self.mana < 0:
+            self.mana = 0
+        if self.mana > self.max_mana:
+            self.mana = self.max_mana
+
     def ma_on_release(self):
-        pass
+        self.create_buff('test')
+        print('Mate.ma_on_release called')
 
-    def create_move_buttons(self):
-        for child in self.parent.children:
-            if type(child) is Mate:
+    def create_buff(self, mode):
+        print('Mate.create_buff called')
+        self.add_widget(Buff(mode))
+
+    def remove_buff(self, buff):
+        print('Mate.remove_buff called')
+        self.remove_widget(buff)
+
+    def create_select_button(self, source, ability):
+        self.add_widget(SelectButton(source, ability))
+
+    def create_select_buttons(self, ability):
+        ''' create select buttons based on the ability used, introduction of a range parameter could be useful to categorize abilities '''
+        index = self.parent.children[:].index(self)
+
+        if  ability == 'move' or ability == 'attack':
+            reach = 'direct'
+        elif ability == 'knightsmove':
+            reach = 'knight'
+        else:
+            reach = 'infinite'
+
+        if reach == 'direct':
+            index_list = [index+1, index-1, index+10, index-10]
+            index_list = [i for i in index_list if i >= 0 and i < 100] # remove upper and lower borders
+            try:
+                if index%10 == 0: # remvoe left border
+                    index_list.remove(index-1)
+                if index%10 == 9: # remove right border
+                    index_list.remove(index+1)
+            except ValueError:
                 pass
-            if type(child) is EmptyField:
-                child.create_select_button(self, 'move')
 
-    def move(self, target):
-        self.parent.switch_positions_by_ref(self, target)
+        elif reach == 'knight':
+            index_list = [index+12, index-12, index+8, index-8, index+21, index-21, index+19, index-19]
+            #do this later, it is frustrating
+        elif reach == 'infinite':
+            index_list = range(0, 100)
+
+        for i in index_list:
+            child = self.parent.children[i]
+            if ability == 'move' or ability == 'knightsmove' and type(child) is EmptyField:
+                child.create_select_button(self, ability)
+            if ability == 'attack' and type(child) is Mate:
+                child.create_select_button(self, ability)
+
+    def start_ability(self, ability):
+        self.create_select_buttons(ability)
+
+    def end_ability(self, ability, target):
+        if ability == 'move' or 'knightsmove':
+            self.parent.switch_positions_by_ref(self, target)
+        elif ability == 'attack':
+            target.change_health(50, 0)
         self.end_turn()
 
     def start_turn(self):
         game = App.get_running_app().root
         menu = game.ids['ability_menu']
-        menu.create_ability_prompt()
-        menu.create_ability_prompt()
-        menu.create_ability_prompt()
-        self.create_move_buttons()
+        menu.create_ability_prompt(self, 'move')
+        menu.create_ability_prompt(self, 'attack')
+        menu.create_ability_prompt(self, 'knightsmove')
+        menu.create_ability_prompt(self, 'test2')
 
     def end_turn(self):
         ''' end the turn by resetting t and game.is_running, and removing all AbilityPrompts '''
@@ -82,6 +143,28 @@ class Mate(Button):
             except AttributeError:
                 pass
 
+    def die(self):
+        ''' called when the mate dies '''
+        self.parent.remove_mate(self)
+
+class Buff(Widget):
+    ''' a widget used to save permanent and temporary changes (buffs/debuffs) on a Mate '''
+    t = 100.
+    stacks = 0
+    def __init__(self, mode, **kwargs):
+        super().__init__(**kwargs)
+        self.mode = mode
+
+    def remove_buff(self):
+        self.parent.remove_buff(self)
+
+    def update(self, *args):
+        game = App.get_running_app().root
+        if game.is_running:
+            self.t -= 1
+            if self.t < 0.:
+                self.remove_buff()
+
 class SelectButton(Button):
     ''' a button to select a target, either an EmptyField or a Mate '''
     def __init__(self, source, ability, **kwargs):
@@ -90,13 +173,12 @@ class SelectButton(Button):
         self.ability = ability
 
     def sb_on_release(self):
-        if self.ability == 'move':
-            self.source.move(self.parent)
+        self.source.end_ability(self.ability, self.parent)
 
 class EmptyField(RelativeLayout):
     ''' an empty field in the playing field '''
     def ef_on_release(self):
-        pass
+        print('EmptyField.ef_on_release called')
 
     def create_select_button(self, source, ability):
         self.add_widget(SelectButton(source, ability))
@@ -110,16 +192,17 @@ class BasicBoxLayout(BoxLayout):
                 pass
 
 class AbilityMenu(BoxLayout):
-    def create_ability_prompt(self):
-        self.add_widget(AbilityPrompt('test ability'))
+    def create_ability_prompt(self, source, ability):
+        self.add_widget(AbilityPrompt(source, ability))
 
 class AbilityPrompt(RelativeLayout):
-    ability = StringProperty('init')
-    def __init__(self, ability, **kwargs):
+    ability = StringProperty('')
+    def __init__(self, source, ability, **kwargs):
         super().__init__(**kwargs)
         self.ability = ability
+        self.source = source
     def ap_on_release(self):
-        pass
+        self.source.start_ability(self.ability)
 
 class PlayingField(GridLayout):
     ''' the playing field where MagicMates move around '''
@@ -130,8 +213,9 @@ class PlayingField(GridLayout):
         self.cols = 10
         for i in range(0, self.cols**2):
             self.add_widget(EmptyField())
-        self.create_mate(5)
-        self.create_mate(6)
+        self.create_mate(15)
+        self.create_mate(26)
+        self.create_mate(16)
 
     def switch_positions(self, index1, index2):
         ''' switch positions of two children '''
@@ -146,6 +230,13 @@ class PlayingField(GridLayout):
         self.add_widget(Mate())
         self.switch_positions(index, 0)
         self.remove_widget(self.children[0])
+
+    def remove_mate(self, mate):
+        ''' remove a Mate by adding an EmptyField, switching it in, and removing the Mate '''
+        empty_field = EmptyField()
+        self.add_widget(empty_field)
+        self.switch_positions_by_ref(empty_field, mate)
+        self.remove_widget(mate)
 
     def update(self, *args):
         game = App.get_running_app().root
